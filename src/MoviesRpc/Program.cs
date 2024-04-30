ae362354b75cac46f01e791447075a2f8f142daa
Jan Einar Ingier <jei@katode.no>
2024-04-30 09:35:48 +0200

    La til Reflection, samt oppgradert nuget pakker

--- src/MoviesRpc/Program.cs
+++ src/MoviesRpc/Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCors();
 builder.Services.AddGrpc().AddJsonTranscoding();
+builder.Services.AddGrpcReflection();
+
 
 var connectionString = builder.Configuration.GetConnectionString("Sqlite");
 
@@ -28,6 +30,8 @@ app.UseRouting();
 app.UseGrpcWeb();
 
 app.MapGrpcService<MoviesImpl>().EnableGrpcWeb();
+app.MapGrpcReflectionService();
+
 
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
